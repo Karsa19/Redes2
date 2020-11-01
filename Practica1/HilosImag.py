be0c10623d16670f2660e2d45f14c8cea9543ea4
Karsa19 <38262267+Karsa19@users.noreply.github.com>
2020-10-31 19:08:29 -0600

    Update HilosImag.py
    Creación de hilos. Aún falta poner el número de imágenes guardadas en el directorio para indicarlo en el for.

--- Practica1/HilosImag.py
+++ Practica1/HilosImag.py
@@ -1,12 +1,14 @@
+import threading
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
 
-#filepath="./" #The current directory
-#filename=""  Nombre del archivo
 
-#pic=Image.open("/content/drive/My Drive/Colab Notebooks/oso1.jpg") #Ruta del archivo
+for i in range():
+    pic=Image.open("./imagenes/" + i +".jpeg")
+    t = threading.Thread(name="hilo"+i, target=convertirImagen(pic, i))
+
 
 
 
@@ -17,7 +19,9 @@ def imagenArray(pic):
     return pic_arr
 
 
-def convertirImagen(Pic):
+def convertirImagen(pic, i):
+
+    pic_arr= imagenArray(pic)
     
     #Convertir imagen a RED
     pic_red=pic_arr.copy()
@@ -25,6 +29,8 @@ def convertirImagen(Pic):
     pic_red[:,:,2]=0
 
     #guardarla
+    red= Image.fromarray(pic_red)
+    red.save("./imagenes/"+ i +"red.jpeg")
 
 
     #Convertir imagen a GREEN
@@ -32,10 +38,18 @@ def convertirImagen(Pic):
     pic_green[:,:,0]=0
     pic_green[:,:,2]=0
 
+    #guardarla
+    green= Image.fromarray(pic_red)
+    green.save("./imagenes/"+ i +"green.jpeg")
+
     #Convertir imagen a BLUE
     pic_blue=pic_arr.copy()
     pic_blue[:,:,0]=0
     pic_blue[:,:,1]=0
+
+    #guardarla
+    blue= Image.fromarray(pic_red)
+    blue.save("./imagenes/"+ i +"blue.jpeg")
 
 
 
